3d42a4eae0ddf4d6d2ed477b6932e3b6f5c5eaf1
Clay <claymcintosh@gmail.com>
2022-03-04 15:14:05 +1000

    Setting up files

---
+++ server.js
@@ -0,0 +1,31 @@
+//dependencies
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
+const consoleTable = require("console.table");
+
+
+// create the connection to database
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    // database: 'rootpass'
+  });
+
+   
+// // simple query
+// connection.query(
+//     'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
+//     function(err, results, fields) {
+//       console.log(results); // results contains rows returned by server
+//       console.log(fields); // fields contains extra meta data about results, if available
+//     }
+//   );
+  
+//   // with placeholder
+//   connection.query(
+//     'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
+//     ['Page', 45],
+//     function(err, results) {
+//       console.log(results);
+//     }
+//   );
